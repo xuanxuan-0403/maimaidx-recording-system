00363e59918eeed47dc64e120d55f10da31e1632
xuanxuan-0403 <1302030435@qq.com>
2023-10-09 14:39:24 +0800

    fix(main/record): 修复不同用户同时操作录制出现冲突的bug

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -5,7 +5,8 @@
             <h2 class="rating">{{ rating }}</h2>
         </header>
         <article :class="{ center: true, active: isStataRecord }" @click="btnClick">
-            <p class="text">{{ recordText }}</p>
+            <p class="text">点击开始录制</p>
+            <p class="time">{{ recordText }}</p>
         </article>
     </div>
 </template>
@@ -20,21 +21,32 @@ export default defineComponent({
     setup() {
         const nickname = ref<string>(login.data.nickname);
         const rating = ref<number>(login.data.rating);
-
         const isStataRecord = ref<Boolean>(false);
         const recordText = ref<string>('点击开始录制');
+
+        // 全局监听其他用户是否在录制
+        let publicRecordActiveTimer = setInterval(() => {
+            obs.call('GetRecordStatus').then((res) => {
+                recordText.value = res.outputTimecode;
+                if (res.outputActive) {
+                    isStataRecord.value = true;
+                } else {
+                    isStataRecord.value = false;
+                }
+            });
+        }, 1000);
+
         const btnClick = () => {
             if (!isStataRecord.value) {
                 isStataRecord.value = true;
-                obs.call('StartRecord').then((res) => {
-                    console.log('开始录制');
-                    recordText.value = '录制中';
-                });
+                obs.call('StartRecord');
             } else {
                 isStataRecord.value = false;
                 obs.call('StopRecord').then((res) => {
                     console.log('停止录制');
-                    recordText.value = '停止录制...';
+                });
+                obs.call('GetRecordStatus').then((res) => {
+                    console.log(res);
                 });
             }
         };
@@ -52,13 +64,12 @@ export default defineComponent({
 <style scoped lang="less">
 .main {
     display: flex;
-    // justify-content: center;
     flex-direction: column;
     align-items: center;
     width: 100vw;
     height: 100vh;
     background: url(../../assets/images/penson.png) center 30% / 120% no-repeat,
-        url('../../assets/images/main-bgc.png') no-repeat 0 center / 120%;
+        url('../../assets/images/main-bgc.png') no-repeat 0 center / 130%;
 
     .header {
         margin-top: 5%;
@@ -68,6 +79,7 @@ export default defineComponent({
 
     .center {
         display: flex;
+        flex-direction: column;
         width: 65vw;
         height: 65vw;
         background: url('../../assets/images/btn.png') no-repeat center center / 100%;
@@ -77,6 +89,7 @@ export default defineComponent({
         p {
             font-weight: bold;
             font-size: 1.4em;
+            margin: 0;
         }
     }
 }
